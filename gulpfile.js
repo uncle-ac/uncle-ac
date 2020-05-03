53e909130adab3ec1cad45e9ba3859932e6984be
uncleacc <601162956@qq.com>
2020-05-03 15:45:57 +0800

    Site updated: 2020-05-03 15:45:56

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
-// 引入需要的模块
 var gulp = require('gulp');
 var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
 var imagemin = require('gulp-imagemin');
+var babel = require('gulp-babel');
 
-// 压缩public目录下所有html文件, minify-html是任务名, 设置为default，启动gulp压缩的时候可以省去任务名
-gulp.task('minify-html', function() {
-    return gulp.src('./public/**/*.html') // 压缩文件所在的目录
+// 压缩css文件
+gulp.task('minify-css', function (done) {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss())
+        .pipe(gulp.dest('./public'));
+    done();
+});
+
+// 压缩html文件
+gulp.task('minify-html', function (done) {
+    return gulp.src('./public/**/*.html')
         .pipe(htmlclean())
         .pipe(htmlmin({
             removeComments: true,
@@ -16,41 +24,60 @@ gulp.task('minify-html', function() {
             minifyCSS: true,
             minifyURLs: true,
         }))
-        .pipe(gulp.dest('./public')) // 输出的目录
+        .pipe(gulp.dest('./public'));
+    done();
 });
 
-// 压缩css
-gulp.task('minify-css', function() {
-    return gulp.src('./public/**/*.css')
-        .pipe(minifycss({
-            compatibility: 'ie8'
+// 压缩js文件
+gulp.task('minify-js', function (done) {
+    return gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
+        .pipe(babel({
+            //将ES6代码转译为可执行的JS代码
+            presets: ['es2015'] // es5检查机制
         }))
-        .pipe(gulp.dest('./public'));
-});
-// 压缩js
-gulp.task('minify-js', function() {
-    return gulp.src(['./public/**/.js','!./public/js/**/*min.js'])
         .pipe(uglify())
         .pipe(gulp.dest('./public'));
+    done();
 });
-// 压缩图片
-gulp.task('minify-images', function() {
-    return gulp.src(['./public/**/*.png','./public/**/*.jpg','./public/**/*.gif'])
-        .pipe(imagemin(
-        [imagemin.gifsicle({'optimizationLevel': 3}), 
-        imagemin.jpegtran({'progressive': true}), 
-        imagemin.optipng({'optimizationLevel': 7}), 
-        imagemin.svgo()],
-        {'verbose': true}))
-        .pipe(gulp.dest('./public'))
+
+// 压缩 public/images 目录内图片(Version<3)
+// gulp.task('minify-images', function () {
+//     gulp.src('./public/images/**/*.*')
+//         .pipe(imagemin({
+//             optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+//             progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+//             interlaced: false, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+//             multipass: false, //类型：Boolean 默认：false 多次优化svg直到完全优化
+//         }))
+//         .pipe(gulp.dest('./public/images'));
+// });
+
+// 压缩 public/images 目录内图片(Version>3)
+gulp.task('minify-images', function (done) {
+    gulp.src('./public/images/**/*.*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            // imagemin.jpegtran({progressive: true}), // 版本升级，改用下面这个
+            imagemin.mozjpeg({progressive: true,}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('./public/images'));
+    done();
 });
-// 默认任务
-/*
-gulp.task('default', [
-    'minify-html','minify-html1','minify-css','minify-js','minify-images'
-]);
-*/
-// gulp 4.0 适用的方式
-gulp.task('default', gulp.parallel('minify-html','minify-css','minify-js','minify-images'
- //build the website
-));
+
+//4.0以前的写法 
+//gulp.task('default', [
+//  'minify-html', 'minify-css', 'minify-js', 'minify-images'
+//]);
+//4.0以后的写法
+// 执行 gulp 命令时执行的任务
+gulp.task('default', gulp.series(gulp.parallel('minify-html', 'minify-css', 'minify-js', 'minify-images')), function () {
+    console.log("----------gulp Finished----------");
+    // Do something after a, b, and c are finished.
+});
--- source/about/index.md
+++ source/about/index.md
@@ -9,14 +9,14 @@ photos: https://cdn.jsdelivr.net/gh/uncleacc/img@1.3/custom/0 (2).jpg
 {% raw %}
 <!-- 因为vue和botui更新导至bug,现将对话移至js下的botui中配置 -->
 <div class="entry-content">
-  <div class="moe-mashiro" style="text-align:center; font-size: 50px; margin-bottom: 20px;">[さくら荘のhojun]</div>
+  <div class="moe-mashiro" style="text-align:center; font-size: 50px; margin-bottom: 20px;">[さくら荘のUncleacc]</div>
   <div id="hello-mashiro" class="popcontainer" style="min-height: 300px; padding: 2px 6px 4px; background-color: rgba(242, 242, 242, 0.5); border-radius: 10px;">
     <center>
     <p>
     </p>
     <h4>
     与&nbsp;<ruby>
-    Mashiro&nbsp;<rp>
+    Uncleacc&nbsp;<rp>
     （</rp>
     <rt>
     真（ま）白（しろ）</rt>
